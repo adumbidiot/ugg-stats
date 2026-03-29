2dd491cbfc91b103a82a6140ac204f5d5642b015
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2026-03-28 22:33:08 -0700

    Refactor main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import curl_cffi
 import pandas as pd
 import typer
@@ -13,6 +15,27 @@ SKIP_QUEUE_TYPES = {
     "urf",
     "normal_aram",
 }
+ALL_SEASONS = [
+    Season.Season16,
+    Season.Season15,
+    Season.Season14_3,
+    Season.Season14_2,
+    Season.Season14_1,
+    Season.Season13_2,
+    Season.Season13_1,
+    Season.Season12,
+    Season.Season11,
+    Season.Season10,
+    Season.Season9,
+    Season.Season8,
+    Season.Season7,
+    Season.Season6,
+    Season.Season5,
+    Season.Season4,
+    Season.Season3,
+    Season.Season2,
+    Season.Season1,
+]
 
 
 def parse_formatted_name(formatted_name: str) -> User:
@@ -20,11 +43,78 @@ def parse_formatted_name(formatted_name: str) -> User:
     return User(name=name, tag=tag)
 
 
+def update_match_summaries(database: Database, user: User, seasons: list[Season]):
+    with curl_cffi.Session() as session:
+        for match_summaries in iter_match_summaries(
+            session, user.name, user.tag, seasons
+        ):
+            for website_summary in match_summaries.match_summaries:
+                summary = MatchSummary.from_website_match_summary(website_summary)
+                database.upsert_match_summary(summary)
+
+
+@dataclass
+class Stats:
+    df: pd.DataFrame
+    total_wins: int = 0
+    total_losses: int = 0
+    total_kills: int = 0
+    total_deaths: int = 0
+    total_assists: int = 0
+
+
+def get_stats_for_user(
+    database: Database, user: User, skip_games_with_user: User | None = None
+) -> Stats:
+    data_map = dict()
+    stats = Stats(df=pd.DataFrame())
+    for summary in database.list_match_summaries(user):
+        if summary.queue_type in SKIP_QUEUE_TYPES:
+            continue
+        assert summary.queue_type == "ranked_solo_5x5", (
+            f'Unknown queue type "{summary.queue_type}"'
+        )
+
+        if skip_games_with_user is not None and summary.has_user(skip_games_with_user):
+            continue
+
+        if summary.win:
+            stats.total_wins += 1
+        else:
+            stats.total_losses += 1
+        stats.total_kills += summary.kills
+        stats.total_deaths += summary.deaths
+        stats.total_assists += summary.assists
+
+        team = summary.get_team_for_user(user)
+        assert team is not None, "Not in team a or b"
+
+        for team_user in team:
+            if team_user.id == user.id:
+                continue
+
+            team_user_formatted_name = f"{team_user.name}#{team_user.tag}"
+
+            entry = data_map.setdefault(
+                team_user_formatted_name,
+                {"User": team_user_formatted_name, "Wins": 0, "Losses": 0},
+            )
+            if summary.win:
+                entry["Wins"] += 1
+            else:
+                entry["Losses"] += 1
+
+    stats.df = pd.DataFrame(data_map.values(), columns=["User", "Wins", "Losses"])
+
+    return stats
+
+
 def main(
     formatted_name: str,
     min_total: int = 3,
     cached: bool = False,
     skip_games_with_user: str | None = None,
+    # seasons: list[str] = [],
 ) -> None:
     user = parse_formatted_name(formatted_name)
 
@@ -32,105 +122,34 @@ def main(
     if skip_games_with_user is not None:
         skip_games_with_user_parsed = parse_formatted_name(skip_games_with_user)
 
-    seasons = [
-        Season.Season16,
-        Season.Season15,
-        Season.Season14_3,
-        Season.Season14_2,
-        Season.Season14_1,
-        Season.Season13_2,
-        Season.Season13_1,
-        Season.Season12,
-        Season.Season11,
-        Season.Season10,
-        Season.Season9,
-        Season.Season8,
-        Season.Season7,
-        Season.Season6,
-        Season.Season5,
-        Season.Season4,
-        Season.Season3,
-        Season.Season2,
-        Season.Season1,
-    ]
+    parsed_seasons = ALL_SEASONS
 
     with Database("database.db") as database:
         database.upsert_user(user)
 
         if not cached:
-            with curl_cffi.Session() as session:
-                for match_summaries in iter_match_summaries(
-                    session, user.name, user.tag, seasons
-                ):
-                    for website_summary in match_summaries.match_summaries:
-                        summary = MatchSummary.from_website_match_summary(
-                            website_summary
-                        )
-                        database.upsert_match_summary(summary)
+            update_match_summaries(database, user, parsed_seasons)
 
-        data_map = dict()
-        total_wins = 0
-        total_losses = 0
-        total_kills = 0
-        total_deaths = 0
-        total_assists = 0
-        for summary in database.list_match_summaries(user):
-            if summary.queue_type in SKIP_QUEUE_TYPES:
-                continue
-            assert summary.queue_type == "ranked_solo_5x5", (
-                f'Unknown queue type "{summary.queue_type}"'
-            )
+        stats = get_stats_for_user(database, user, skip_games_with_user_parsed)
 
-            if skip_games_with_user_parsed is not None and summary.has_user(
-                skip_games_with_user_parsed
-            ):
-                continue
+    stats.df["Total"] = stats.df["Wins"] + stats.df["Losses"]
+    stats.df["Win %"] = stats.df["Wins"] / stats.df["Total"]
+    stats.df = stats.df.sort_values(by="Win %", ascending=False)
+    stats.df = stats.df[stats.df["Total"] >= min_total]
 
-            if summary.win:
-                total_wins += 1
-            else:
-                total_losses += 1
-            total_kills += summary.kills
-            total_deaths += summary.deaths
-            total_assists += summary.assists
+    print(stats.df.to_string(index=False))
 
-            team = summary.get_team_for_user(user)
-            assert team is not None, "Not in team a or b"
-
-            for team_user in team:
-                if team_user.id == user.id:
-                    continue
-
-                team_user_formatted_name = f"{team_user.name}#{team_user.tag}"
-
-                entry = data_map.setdefault(
-                    team_user_formatted_name,
-                    {"User": team_user_formatted_name, "Wins": 0, "Losses": 0},
-                )
-                if summary.win:
-                    entry["Wins"] += 1
-                else:
-                    entry["Losses"] += 1
-
-    df = pd.DataFrame(data_map.values(), columns=["User", "Wins", "Losses"])
-    df["Total"] = df["Wins"] + df["Losses"]
-    df["Win %"] = df["Wins"] / df["Total"]
-    df = df.sort_values(by="Win %", ascending=False)
-    df = df[df["Total"] >= min_total]
-
-    print(df.to_string(index=False))
-
-    total_matches = total_wins + total_losses
+    total_matches = stats.total_wins + stats.total_losses
     print()
-    print(f"Total Wins: {total_wins}")
-    print(f"Total Losses: {total_losses}")
+    print(f"Total Wins: {stats.total_wins}")
+    print(f"Total Losses: {stats.total_losses}")
     print(f"Total Matches: {total_matches}")
-    print(f"Total Win %: {total_wins / total_matches}")
-    print(f"Total Kills: {total_kills}")
-    print(f"Total Deaths: {total_deaths}")
-    print(f"Total Assists: {total_assists}")
+    print(f"Total Win %: {stats.total_wins / total_matches:.3f}")
+    print(f"Total Kills: {stats.total_kills}")
+    print(f"Total Deaths: {stats.total_deaths}")
+    print(f"Total Assists: {stats.total_assists}")
     print(
-        f"Average K/D/A: {total_kills / total_matches:.3f}/{total_deaths / total_matches:.3f}/{total_assists / total_matches:.3f}"
+        f"Average K/D/A: {stats.total_kills / total_matches:.3f}/{stats.total_deaths / total_matches:.3f}/{stats.total_assists / total_matches:.3f}"
     )
 
 
